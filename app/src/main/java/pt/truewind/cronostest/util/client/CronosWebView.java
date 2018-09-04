7563dbecd7c93367d763fc422e39d7cf742827e7
EricJoosse <eric.jo@bol.com.br>
2018-09-04 13:55:08 -0300

    Remoção e adicionamento de diversos botões no teclado Android

--- app/src/main/java/pt/truewind/cronostest/util/client/CronosWebView.java
+++ app/src/main/java/pt/truewind/cronostest/util/client/CronosWebView.java
@@ -37,6 +37,25 @@ public class CronosWebView extends WebView {
         // para decidir o tipo de teclado automaticamente:
     //  outAttrs.inputType = InputType.TYPE_CLASS_NUMBER;
 
+
+        // Para resolver o bug que o botão "Próximo campo" não aparece no teclado em alguns casos:
+        // (veja https://stackoverflow.com/questions/16686681/soft-keyboard-in-webview-no-next-button-to-tab-between-input-fields)
+        if (outAttrs != null) {
+            // Remover outros botões que não se aplicam nas telas exibidas dentro do WebView para liberar espaço no teclado:
+            outAttrs.imeOptions &= ~EditorInfo.IME_ACTION_GO;
+            outAttrs.imeOptions &= ~EditorInfo.IME_ACTION_SEARCH;
+            outAttrs.imeOptions &= ~EditorInfo.IME_ACTION_SEND;
+            outAttrs.imeOptions &= ~EditorInfo.IME_ACTION_DONE;
+            outAttrs.imeOptions &= ~EditorInfo.IME_ACTION_NONE;
+
+            // Adicionar o botão "Próximo":
+            outAttrs.imeOptions |= EditorInfo.IME_ACTION_NEXT;
+
+
+            // Para resolver o bug que o botão "Fechar teclado" também não aparece no teclado em alguns casos:
+            outAttrs.imeOptions |= EditorInfo.IME_ACTION_DONE;
+        }
+
         return ic;
     }
 
